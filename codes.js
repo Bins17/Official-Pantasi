887b69d9e7078fb13a3cd9eec0a41b73c0505fc5
Bins17 <146843777+Bins17@users.noreply.github.com>
2023-12-12 18:48:06 +0800

    Update codes.js

--- codes.js
+++ codes.js
@@ -465,7 +465,7 @@ function updateBudgetLeft() {
 
 
 function updateCountdown() {
-          const targetDate = new Date('2023-12-11T22:40:00');
+          const targetDate = new Date('2023-12-12T21:30:00');
 const now = new Date().getTime();
 const distance = targetDate - now;
 
